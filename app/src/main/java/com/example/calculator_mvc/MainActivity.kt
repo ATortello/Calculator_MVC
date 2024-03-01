2dea997b61f6b76cd790e4e4feb6bb3be0a439d1
Agustin R Tortello D <arafaelt01@gmail.com>
2024-03-01 18:17:58 -0500

    005. Updating logic for calculator v1.1 - Pending buttons - 1

--- app/src/main/java/com/example/calculator_mvc/MainActivity.kt
+++ app/src/main/java/com/example/calculator_mvc/MainActivity.kt
@@ -2,47 +2,47 @@ package com.example.calculator_mvc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    var valueMainExpression: String = ""
-    var valueResult: String = ""
-    var operation: String = ""
-    var operator: Int = 0
-    var dotFlag: Int = 0
-    var num1: Double = 0.0
-    var num2: Double = 0.0
+    private var valueMainExpression: String = ""
+    private var valueResult: String = ""
+    private var value: String = ""
+    private var operation: String = ""
+    private var operator: Int = 0
+    private var dotFlag: Int = 0
+    private var num1: Double = 0.0
+    private var num2: Double = 0.0
 
     /*TextViews to show information*/
-    lateinit var tvMainExpression: TextView
-    lateinit var tvMainCurrentResults: TextView
+    private lateinit var tvMainExpression: TextView
+    private lateinit var tvMainCurrentResults: TextView
 
     /*Operation Buttons*/
-    lateinit var btMainClear: Button
-    /*lateinit var btMainParenthesis: Button
-    lateinit var btMainPercentage: Button*/
-    lateinit var btMainDivision: Button
-    lateinit var btMainMultiplication: Button
-    lateinit var btMainMinus: Button
-    lateinit var btMainPlus: Button
-    lateinit var btMainEqual: Button
-    lateinit var btMainRet: Button
+    private lateinit var btMainClear: Button
+    /*private lateinit var btMainParenthesis: Button
+    private lateinit var btMainPercentage: Button*/
+    private lateinit var btMainDivision: Button
+    private lateinit var btMainMultiplication: Button
+    private lateinit var btMainMinus: Button
+    private lateinit var btMainPlus: Button
+    private lateinit var btMainEqual: Button
+    private lateinit var btMainRet: Button
 
     /*All Number Buttons*/
-    lateinit var btMainDot: Button
-    lateinit var btMainZero: Button
-    lateinit var btMainOne: Button
-    lateinit var btMainTwo: Button
-    lateinit var btMainThree: Button
-    lateinit var btMainFour: Button
-    lateinit var btMainFive: Button
-    lateinit var btMainSix: Button
-    lateinit var btMainSeven: Button
-    lateinit var btMainEight: Button
-    lateinit var btMainNine: Button
+    private lateinit var btMainDot: Button
+    private lateinit var btMainZero: Button
+    private lateinit var btMainOne: Button
+    private lateinit var btMainTwo: Button
+    private lateinit var btMainThree: Button
+    private lateinit var btMainFour: Button
+    private lateinit var btMainFive: Button
+    private lateinit var btMainSix: Button
+    private lateinit var btMainSeven: Button
+    private lateinit var btMainEight: Button
+    private lateinit var btMainNine: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -79,8 +79,8 @@ class MainActivity : AppCompatActivity() {
             num1 = 0.0
             num2 = 0.0
             dotFlag = 0
-            tvMainExpression.setText("")
-            tvMainCurrentResults.setText("")
+            tvMainExpression.text = ""
+            tvMainCurrentResults.text = ""
         }
 
         btMainDivision.setOnClickListener{
@@ -88,7 +88,7 @@ class MainActivity : AppCompatActivity() {
             num1 = valueMainExpression.toDouble()
             tvMainCurrentResults.text = ""
 
-            operation = "$valueMainExpression/"
+            operation = "${valueMainExpression}/"
             tvMainExpression.text = operation
             operator = 4
         }
@@ -98,7 +98,7 @@ class MainActivity : AppCompatActivity() {
             num1 = valueMainExpression.toDouble()
             tvMainCurrentResults.text = ""
 
-            operation = "$valueMainExpression&#215;"
+            operation = "${valueMainExpression}X"
             tvMainExpression.text = operation
             operator = 3
         }
@@ -108,7 +108,7 @@ class MainActivity : AppCompatActivity() {
             num1 = valueMainExpression.toDouble()
             tvMainCurrentResults.text = ""
 
-            operation = "$valueMainExpression-"
+            operation = "${valueMainExpression}-"
             tvMainExpression.text = operation
             operator = 2
         }
@@ -118,7 +118,7 @@ class MainActivity : AppCompatActivity() {
             num1 = valueMainExpression.toDouble()
             tvMainCurrentResults.text = ""
 
-            operation = "$valueMainExpression+"
+            operation = "${valueMainExpression}+"
             tvMainExpression.text = operation
             operator = 1
         }
@@ -130,21 +130,25 @@ class MainActivity : AppCompatActivity() {
                 1 -> valueResult = (num1 + num2).toString()
                 2 -> valueResult = (num1 - num2).toString()
                 3 -> valueResult = (num1 * num2).toString()
-                4 -> valueResult = (num1 / num2).toString()
+                4 -> valueResult = if (num2 == 0.0) "Can't divide by 0"
+                else (num1 / num2).toString()
             }
 
-            tvMainCurrentResults.setText(valueResult)
-            tvMainExpression.setText("")
+            tvMainCurrentResults.text = valueResult
+            tvMainExpression.text = ""
         }
 
         btMainRet.setOnClickListener {
-            //Toast.makeText(this, "Digits Qty: ${tvMainCurrentResults.length()}", Toast.LENGTH_LONG).show()
-            val aux1: Int = tvMainCurrentResults.text.get(tvMainCurrentResults.length()-1).digitToInt()
-
-            val aux2: String = (tvMainCurrentResults.text.toString().toDouble() - aux1.toDouble()).div(10).toInt().toString()
-            //Toast.makeText(this, "Digit removed: $aux1", Toast.LENGTH_LONG).show()
-
-            tvMainCurrentResults.text = aux2
+//            Toast.makeText(this, "Digits Qty: ${tvMainCurrentResults.length()}", Toast.LENGTH_LONG).show()
+//            val aux1: Int = tvMainCurrentResults.text
+//                .get(tvMainCurrentResults.length()-1)
+//                .digitToInt()
+//
+//            val aux2: String = (tvMainCurrentResults.text.toString().toDouble() - aux1.toDouble())
+//                .div(10).toInt().toString()
+//            Toast.makeText(this, "Digit removed: $aux1", Toast.LENGTH_LONG).show()
+//
+//            tvMainCurrentResults.text = aux2
         }
 
         /*==== Definition for each digit button ====*/
@@ -165,100 +169,61 @@ class MainActivity : AppCompatActivity() {
         }
 
         btMainZero.setOnClickListener {
-            val zeroValue: String
             if(tvMainCurrentResults.text.isNotEmpty()) {
-                zeroValue = tvMainCurrentResults.text.toString() + "0"
-                tvMainCurrentResults.text = zeroValue
+                value = tvMainCurrentResults.text.toString() + "0"
+                tvMainCurrentResults.text = value
             }
-            else { Toast.makeText(this, "No initial zeros allowed!", Toast.LENGTH_SHORT).show() }
+            else {
+                if (operator == 4) tvMainCurrentResults.text = "0"
+                else Toast
+                    .makeText(this, "No initial zeros allowed!", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
 
         btMainOne.setOnClickListener {
-            val oneValue: String = tvMainCurrentResults.text.toString() + "1"
-            tvMainCurrentResults.text = oneValue
+            value = tvMainCurrentResults.text.toString() + "1"
+            tvMainCurrentResults.text = value
         }
 
         btMainTwo.setOnClickListener {
-            val twoValue: String = tvMainCurrentResults.text.toString() + "2"
-            tvMainCurrentResults.text = twoValue
+            value = tvMainCurrentResults.text.toString() + "2"
+            tvMainCurrentResults.text = value
         }
 
         btMainThree.setOnClickListener {
-            val threeValue: String = tvMainCurrentResults.text.toString() + "3"
-            tvMainCurrentResults.text = threeValue
+            value = tvMainCurrentResults.text.toString() + "3"
+            tvMainCurrentResults.text = value
         }
 
         btMainFour.setOnClickListener {
-            val fourValue: String = tvMainCurrentResults.text.toString() + "4"
-            tvMainCurrentResults.text = fourValue
+            value = tvMainCurrentResults.text.toString() + "4"
+            tvMainCurrentResults.text = value
         }
 
         btMainFive.setOnClickListener {
-            val fiveValue: String = tvMainCurrentResults.text.toString() + "5"
-            tvMainCurrentResults.text = fiveValue
+            value = tvMainCurrentResults.text.toString() + "5"
+            tvMainCurrentResults.text = value
         }
 
         btMainSix.setOnClickListener {
-            val sixValue: String = tvMainCurrentResults.text.toString() + "6"
-            tvMainCurrentResults.text = sixValue
+            value = tvMainCurrentResults.text.toString() + "6"
+            tvMainCurrentResults.text = value
         }
 
         btMainSeven.setOnClickListener {
-            val sevenValue: String = tvMainCurrentResults.text.toString() + "7"
-            tvMainCurrentResults.text = sevenValue
+            value = tvMainCurrentResults.text.toString() + "7"
+            tvMainCurrentResults.text = value
         }
 
         btMainEight.setOnClickListener {
-            val eightValue: String = tvMainCurrentResults.text.toString() + "8"
-            tvMainCurrentResults.text = eightValue
+            value = tvMainCurrentResults.text.toString() + "8"
+            tvMainCurrentResults.text = value
         }
 
         btMainNine.setOnClickListener {
-            val nineValue: String = tvMainCurrentResults.text.toString() + "9"
-            tvMainCurrentResults.text = nineValue
+            value = tvMainCurrentResults.text.toString() + "9"
+            tvMainCurrentResults.text = value
         }
     }
-    /*fun pressedKey(view: View){
-        valueMainExpression = tvMainCurrentResults.text.toString()
-
-        when(view.id){
-            R.id.btMainZero -> tvMainCurrentResults.setText(valueMainExpression + "0")
-            R.id.btMainOne -> tvMainCurrentResults.setText(valueMainExpression + "1")
-            R.id.btMainTwo -> tvMainCurrentResults.setText(valueMainExpression + "2")
-            R.id.btMainThree -> tvMainCurrentResults.setText(valueMainExpression + "3")
-            R.id.btMainFour -> tvMainCurrentResults.setText(valueMainExpression + "4")
-            R.id.btMainFive -> tvMainCurrentResults.setText(valueMainExpression + "5")
-            R.id.btMainSix -> tvMainCurrentResults.setText(valueMainExpression + "6")
-            R.id.btMainSeven -> tvMainCurrentResults.setText(valueMainExpression + "7")
-            R.id.btMainEight -> tvMainCurrentResults.setText(valueMainExpression + "8")
-            R.id.btMainNine -> tvMainCurrentResults.setText(valueMainExpression + "9")
-            R.id.btMainDot -> tvMainCurrentResults.setText(valueMainExpression + ".")
-        }
-    }*/
-    /*fun selectOperator(view: View){
-        valueMainExpression = tvMainCurrentResults.text.toString()
-        num1 = valueMainExpression.toDouble()
-        tvMainCurrentResults.text = ""
-
-
-        when(view.id){
-            *//*R.id.btMainClear -> tvMainCurrentResults.setText("0")*//*
-            R.id.btMainPlus -> {
-                tvMainExpression.text = valueMainExpression + "+"
-                operator = 1
-            }
-            R.id.btMainMinus -> {
-                tvMainExpression.text = valueMainExpression + "-"
-                operator = 2
-            }
-            R.id.btMainMultiplication -> {
-                tvMainExpression.text = valueMainExpression + "X"
-                operator = 3
-            }
-            R.id.btMainDivision -> {
-                tvMainExpression.text = valueMainExpression + "/"
-                operator = 4
-            }
-        }
-    }*/
 }
